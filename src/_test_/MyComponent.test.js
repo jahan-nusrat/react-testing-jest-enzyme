865ada120138dbd2be9a20ee1d987a7960e71958
Nusrat Jahan <njnusrat53@gmail.com>
2021-03-08 13:16:41 +0600

    hide text when clicked passed

--- src/_test_/MyComponent.test.js
+++ src/_test_/MyComponent.test.js
@@ -6,6 +6,13 @@ describe('MyComponent', () => {
   it('Should show text', ()=> {
     const wrapper = shallow(<MyComponent />);
     const text = wrapper.find('div p');
-    expect(text.text()).toEqual('Lorem ipsum dolor sit')
+    expect(text.text()).toBe('Lorem ipsum dolor sit')
+  });
+  it('Should hide text when button clicked', () => {
+    const wrapper = shallow(<MyComponent />);
+    const button = wrapper.find('button');
+    button.simulate('click')
+    const text = wrapper.find('div p');
+    expect(text.length).toBe(0)
   })
 })
